9fec652c4310ed58c2ace62874eae9e4a2d18830
wrightsman-nick <77906618+wrightsman-nick@users.noreply.github.com>
2021-10-11 20:02:40 -0400

    audio added

--- 3D Beginner/Assets/Scripts/GameEnding.cs
+++ 3D Beginner/Assets/Scripts/GameEnding.cs
@@ -10,9 +10,12 @@ public class GameEnding : MonoBehaviour
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
     public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public AudioSource exitAudio;
+    public AudioSource caughtAudio;
 
     bool m_IsPlayerAtExit;
     bool m_IsPlayerCaught;
+    bool m_HasAudioPlayed;
     float m_Timer;
 
     void OnTriggerEnter (Collider other)
@@ -32,16 +35,22 @@ public class GameEnding : MonoBehaviour
     {
         if(m_IsPlayerAtExit)
         {
-            EndLevel (exitBackgroundImageCanvasGroup, false);
+            EndLevel (exitBackgroundImageCanvasGroup, false, exitAudio);
         }
         else if(m_IsPlayerCaught)
         {
-            EndLevel (caughtBackgroundImageCanvasGroup, true);
+            EndLevel (caughtBackgroundImageCanvasGroup, true, caughtAudio);
         }
     }
 
-    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart)
+    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
     {
+        if (!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+        }
+
         m_Timer += Time.deltaTime;
 
         imageCanvasGroup.alpha = m_Timer / fadeDuration;
